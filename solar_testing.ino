b19fbaada375d256125917224aaf32fb4ddd707d
kentalin <114200848+kentalin@users.noreply.github.com>
2024-12-03 19:29:04 -0800

    Add files via upload

---
+++ solar_testing.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(0, OUTPUT);
+pinMode(1, OUTPUT);
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(0, LOW);
+digitalWrite(1, HIGH);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(7, HIGH);
+
+}
