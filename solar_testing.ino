e63b4ac246e26d35fcab3d86d526146db7309666
kizoomi <keiraizu@usc.edu>
2024-12-03 20:42:32 -0800

    initial performance testing

--- solar_testing.ino
+++ solar_testing.ino
@@ -1,24 +1,166 @@
 void setup() {
-  // put your setup code here, to run once:
-pinMode(0, OUTPUT);
-pinMode(1, OUTPUT);
-pinMode(2, OUTPUT);
-pinMode(3, OUTPUT);
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-pinMode(6, OUTPUT);
-pinMode(7, OUTPUT);
+  // Initialize serial communication at 9600 baud rate
+  Serial.begin(9600);
+  
+  // Set all the pins as OUTPUT
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(0, LOW);
-digitalWrite(1, HIGH);
-digitalWrite(2, LOW);
-digitalWrite(3, LOW);
-digitalWrite(4, HIGH);
-digitalWrite(5, HIGH);
-digitalWrite(6, HIGH);
-digitalWrite(7, HIGH);
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
+
+
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
+
+
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
+
+
+  
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
+
+  
+  
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
+
+
+  
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
+
+  
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
+
+  
+  
+  // Set pin states
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  
+  // Print the current (analog value read from A5 value between 0 and 1023)
+  Serial.println(analogRead(A5)*0.045870824-24.8466163);
+  
+
+  Serial.println("done");
+  // Add a small delay to make sure the serial output is visible
+  delay(500); // Delay for 500 milliseconds
+
 
 }
